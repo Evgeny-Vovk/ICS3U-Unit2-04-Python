f2b37e9765e4e9da94c64b67640ee5a5b4efcd41
Evgeny Vovk <Evgeny.vovk@mths.ca>
2022-09-29 17:26:02 +0000

    done with unit 2-04 python

---
+++ ICS3U-Unit2-04.py
@@ -0,0 +1,26 @@
+# Copyright (c) 2022 Evgeny Vovk All rights reserved.
+#
+# Created by: Evgeny Vovk
+# Created on: September 2022
+# ICS3U-Unit2-04.py File, learning input process and output in python.
+
+import constants
+
+
+def main():
+
+    # input
+    diameter = int(input("Type in the diameter of your pizza in inches: "))
+
+    # process
+    pizza_cost = constants.LABOR + constants.RENT + (diameter * constants.COST_PER_INCH)
+    total = pizza_cost * constants.HST
+
+    # output
+    print("\nThe cost of the pizza for {0} inches is ${1:,.2f} mm².".format(diameter, total))
+
+    print("\n\nDone.")
+
+
+if __name__ == "__main__":
+    main()
---
+++ constants.py
@@ -0,0 +1,10 @@
+# Copyright (c) 2022 Evgeny Vovk All rights reserved.
+#
+# Created by: Evgeny Vovk
+# Created on: September 2022
+# constants file for ICS3U-Unit2-03-Python.
+
+RENT = 1
+LABOR = 0.75
+COST_PER_INCH = 0.5
+HST = 1.13
